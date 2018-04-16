330425b9cecaeb25219e0a4e6c69cc524737b44b
jonesga1 <gabriel.jones@ucdenver.edu>
2018-04-16 09:39:05 -0600

    Create Dictionary class and add Array of Linked Lists of Strings

---
+++ HashTablesDictionary/Dictionary.h
@@ -0,0 +1,14 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <list>
+#include <string>
+
+class Dictionary {
+private:
+	std::list<std::string> wordList[1373];
+public:
+	Dictionary();
+};
+
+#endif
